14a3db83de0d30c109d604aa4ebc149ea897db90
jeannefo <jeannefoliveira@gmail.com>
2023-03-04 14:04:20 -0300

    criando o projeto

---
+++ src/main/java/com/ead/decoderauthuser/DecoderAuthuserApplication.java
@@ -0,0 +1,13 @@
+package com.ead.decoderauthuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoderAuthuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecoderAuthuserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ead/decoderauthuser/DecoderAuthuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.decoderauthuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoderAuthuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
